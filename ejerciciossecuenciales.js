5f79cf801a3977dc4972a61803fa3a89047bf582
Sebastian <sebasr1268@gmail.com>
2022-11-29 14:02:00 -0500

    segundo commit

---
+++ ejerciciossecuenciales.js
@@ -0,0 +1,29 @@
+console.log("ejercicios de algoritmos secuenciales ")
+console.log("ejercicio 1")
+let A,B,C
+A = 2
+B = 5
+C = 1
+console.log('ejercicio a')
+console.log(3 * A - 4 * B / A**2)
+console.log("ejercicio b")
+console.log((((B + C)/2*A+10)*3*B)-6)
+
+console.log("\nejercicio 2 ")
+let capital,tasa,ganancia
+capital = parseFloat(prompt("Digite cuanto invertio de capital"))
+tasa = 1.2/100
+ganancia = tasa * capital
+console.log(`tendra de ganancia ${ganancia}`)
+
+console.log("\n ejeercicio 3")
+let = altu,base,area
+altu = parseInt(prompt("digite la altura del triangulo  "))
+base = parseInt(prompt("digite la base del triangulo  "))
+area = base*altu/2
+console.log(`el area es de ${area}`)
+
+console.log("\nejercicio 4")
+let = salario,comision,autoV,TotalM
+autoV=parseInt(prompt("digite cuantos autos vendidos son "))
+TotalM=parseFloat(prompt())
